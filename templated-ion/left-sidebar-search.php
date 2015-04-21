bb264b9ad10bebe91bf61e423983d200b71f6a9e
lalaith <salehaiqbalhussain.iitd@gmail.com>
2015-04-21 21:27:48 +0530

    Added Sort
    Finally

--- templated-ion/left-sidebar-search.php
+++ templated-ion/left-sidebar-search.php
@@ -9,14 +9,27 @@ $s0= $_POST['search0'];
 $s1= $_POST['search1'];  
 $s2= $_POST['search2'];     
 $s3= $_POST['search3'];
+$s4= $_POST['search4'];
+$s5= $_POST['search5'];
+$s6= $_POST['search6'];
+$s7= $_POST['search7'];
+$s8= $_POST['search8'];
 
 $stack = array("orange");
 
 
 $sql = "select courses.name as name, courses.shortname as shortname, courses.L as L, courses.T as T, courses.P as P, courses.credits as credits, 
 courses.CID as CID, courses.IID as IID, courses.content as content, courses.slot as slot, instructor.IID as insIID, instructor.name as instructor 
-from courses inner join instructor on courses.IID = instructor.IID WHERE 
-courses.shortname LIKE '%".$s0 ."%".$s1."%' AND courses.name LIKE '%".$s2 ."%' AND instructor.name LIKE '%".$s3 ."%' ";
+from courses inner join instructor on courses.IID = instructor.IID
+inner join 
+(select CID, avg(ar) as ar,avg(gr) as gr,avg(interest) as interest,avg(work) as work,avg(ov) as ov from rating group by CID)
+avgrating
+on avgrating.CID = courses.CID
+WHERE 
+courses.shortname LIKE '%".$s0 ."%".$s1."%' AND courses.name LIKE '%".$s2 ."%' AND instructor.name LIKE '%".$s3 ."%' 
+AND courses.credits >=".$s6." AND courses.credits <= ".$s7."
+AND courses.slot LIKE '".$s8."%'
+ORDER by avgrating.".$s4." ".$s5." ;";
 
 
 //$sql = "SELECT * FROM courses WHERE shortname LIKE 'CSL%';";
